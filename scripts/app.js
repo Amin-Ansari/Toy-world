e704150724add775ac72644b57e97f9dcffd7b3f
Amin Ansari <devansari@yahoo.com>
2022-07-26 14:33:27 +0430

    the basket is done

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,32 @@
 let submenuButton = document.querySelector(".sub-menu-button");
+let basketButton = document.querySelector(".shop-basket");
 
 submenuButton.addEventListener("click", showSubMenu);
+basketButton.addEventListener("click", showBasket);
+document.addEventListener("click", function (eventOb) {
+  if (eventOb.target.classList.contains("basket-stored")) {
+    showBasket();
+  } else if (eventOb.target.nodeName == "I") {
+    showBasket();
+  }
+});
 
 function showSubMenu() {
   barsTransform();
   let theMobileMenu = document.querySelector(".mobile-menu");
   theMobileMenu.classList.toggle("show-sub");
 }
-
 function barsTransform() {
   let allBars = document.querySelectorAll(".bars");
   for (let i = 0; i < allBars.length; i++) {
     allBars[i].classList.toggle("opened");
   }
+}
+function showBasket() {
+  let basketContainer = document.querySelector(".basket-stored");
+  let basketCard = document.querySelector(".basket-cards");
+  basketContainer.classList.toggle("cards-flex");
+  setTimeout(() => {
+    basketCard.classList.toggle("scale-normal");
+  }, 50);
 }
