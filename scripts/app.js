f15b9d7e10fe26c3950d6d864bc7382460400c7a
Amin Ansari <devansari@yahoo.com>
2022-07-31 04:04:03 +0430

    the video section is done

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,14 @@ let categoryItems = document.querySelectorAll(".category-list>li");
 let categoryListItem = document.querySelectorAll(".category-list-items");
 let scrollButton = document.querySelector(".scroll-donw-link");
 let videoDiv = document.querySelector(".video-part");
+let playButton = document.querySelector(".play-icon");
+let container = document.querySelector(".video-container");
+let video = document.querySelector(".video-container>video");
 
 submenuButton.addEventListener("click", showSubMenu);
 basketButton.addEventListener("click", showBasket);
+document.addEventListener("click", puaseVideo);
+
 document.addEventListener("click", function (eventOb) {
   if (eventOb.target.classList.contains("basket-stored")) {
     showBasket();
@@ -27,6 +32,7 @@ scrollButton.addEventListener("click", function () {
   document.documentElement.scrollTop =
     document.documentElement.clientHeight * 0.5;
 });
+playButton.addEventListener("click", playVideo);
 
 function showSubMenu() {
   barsTransform();
@@ -56,6 +62,21 @@ function sectionScale(element) {
   if (elementLocation.top <= 900) {
     if (!element.classList.contains("category-show-up")) {
       element.classList.add("category-show-up");
+    }
+  }
+}
+function playVideo() {
+  container.classList.replace("dis-none", "dis-flex");
+  video.classList.toggle("category-show-up");
+}
+function puaseVideo(eventOb) {
+  let container = document.querySelector(".video-container");
+  if (eventOb.target == container) {
+    video.pause();
+    video.currentTime = 0;
+    container.classList.replace("dis-flex", "dis-none");
+    if (video.classList.contains("category-show-up")) {
+      video.classList.remove("category-show-up");
     }
   }
 }
